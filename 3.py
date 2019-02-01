3c33964db0d460d331c092643e98654a0cdd04b7
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-02-01 10:07:48 +0530

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,7 @@
+vowel=input()
+if(vowel='a' or vowel='A' or vowel='e' or vowel='E' or vowel='i' or vowel='I' or vowel='o' or vowel='O' or vowel='u' or vowel'U'):
+    print("Vowel")
+else:
+    print("Consonant")
+
+
