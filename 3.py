2479d0c31fc534b6c0b84eecb89e4047f8b144ea
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-02-01 10:14:30 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,5 +1,5 @@
-vowel=input()
-if(vowel='a' or vowel='A' or vowel='e' or vowel='E' or vowel='i' or vowel='I' or vowel='o' or vowel='O' or vowel='u' or vowel'U'):
+vowel=raw_input()
+if(vowel=='a' or vowel=='A' or vowel=='e' or vowel=='E' or vowel=='i' or vowel=='I' or vowel=='o' or vowel=='O' or vowel=='u' or vowel=='U'):
     print("Vowel")
 else:
     print("Consonant")
