2a70ab2964cf5c9f3e2f5ef59f5fd767a61f36c2
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-02-04 14:34:02 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,6 +1,7 @@
-first_no=int(input())
-second_no=int(input())
-third_no=int(input())
+num=input().split()
+first_no=int(num[0])
+second_no=int(num[1])
+third_no=int(num[2])
 if(first_no>second_no and first_no>third_no):
     print(first_no)
 elif(second_no>third_no):
