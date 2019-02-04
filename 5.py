002ab79ae28e0aa5895525a1eabdaf73b0213986
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-02-04 14:15:33 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+first_no=int(input())
+second_no=int(input())
+third_no=int(input())
+if(first_no>second_no and first_no>third_no):
+    print(first_no)
+elif(second_no>third_no):
+    print(second_no)
+else:
+    print(third_no)
