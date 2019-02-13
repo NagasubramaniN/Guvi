61408d9816bc850a41202076d9eb1cc8a3479102
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-02-13 14:35:23 +0530

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,6 +1,6 @@
-count = 0
-number = int(raw_input())
-while (number > 0):
-  number = number/10
-  count = count + 1
+count=0
+num=int(raw_input())
+while(num>0):
+    num=num/10
+    count=count+1
 print count
