476b9551bec025f81d98b610f0d23b92b4021978
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-03-24 13:28:46 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -1,11 +1,11 @@
-n=int(input())
-temp=n
-rev=0
-while(n>0):
-    dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
+a=int(input())
+b=a
+c=0
+while(a>0):
+    d=a%10
+    c=c*10+d
+    a=a//10
+if(b==c):
     print("yes")
 else:
     print("no")
