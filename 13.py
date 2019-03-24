6dbe4de8eb5d61bfb11774044d21b06b6dc0f194
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-03-24 14:05:12 +0530

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,9 +1,9 @@
-a=int(input())
-k=0
-for i in range(2,a//2+1):
-    if(a%i==0):
-        k=k+1
-if(k<=0):
+n=int(input())
+num=0
+for i in range(2,n//2+1):
+    if(n%i==0):
+        num=num+1
+if(num<=0):
     print("yes")
 else:
     print("no")
