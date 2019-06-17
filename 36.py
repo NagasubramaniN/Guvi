4eb8437d6fa7ba1390e605e765c64e5ab5955704
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-06-17 17:49:34 +0530

    Update 36.py

--- 36.py
+++ 36.py
@@ -1,9 +1,9 @@
 n=int(input())
 g=list(map(int,input().split()))
-count=0
+cnt=0
 for i in range(len(g)-2):
     for j in range(i+1,len(g)-1):
         for k in range(j+1,len(g)):
             if g[i]<g[j]<g[k] and i<j<k:
-                count=count+1
-print(count)
+                cnt=cnt+1
+print(cnt)
