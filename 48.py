75d047250a579a4c2b5dc6ad3fb8ec587d2528d1
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-06-17 18:58:27 +0530

    Update 48.py

--- 48.py
+++ 48.py
@@ -6,8 +6,8 @@ for i in range(n1p):
 for i in range(n1p):
     for j in range(m1p-1):
         for k in range(j+1,m1p+1):
-            if lp[i][j:k]==[1p]*len(lp[i][j:k]):
-                 if all(lp[p+i][j:k]==[1p]*len(lp[p+i][j:k]) for p in range(len(lp[i][j:k])-1)):
+            if lp[i][j:k]==[1]*len(lp[i][j:k]):
+                 if all(lp[p+i][j:k]==[1]*len(lp[p+i][j:k]) for p in range(len(lp[i][j:k])-1)):
                      if len(lp[i][j:k])>uip:
                         uip=len(lp[i][j:k])
 if len(lp)==1 and len(lp[0])==1 and lp[0][0]==1:
