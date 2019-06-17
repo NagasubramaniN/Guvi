fd87c22540a952e3531c5f8f6f051280375a080f
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-06-17 17:58:01 +0530

    Update 38.py

--- 38.py
+++ 38.py
@@ -4,5 +4,5 @@ inpt11,inpt21=map(int,input().split())
 List=[int(i) for i in input().split()]
 for i in range(inpt21):
     cp,dp=map(int,input().split())
-    tempe=functools.reduce(math.gcd,List[ct-1:dt])
+    tempe=functools.reduce(math.gcd,List[cp-1:dp])
     print(tempe)
