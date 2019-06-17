d6ad5dc0c8069de9a9b76a00b9052f658b2b7226
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-06-17 19:33:49 +0530

    Create 54.py

---
+++ 54.py
@@ -0,0 +1,13 @@
+nump=int(input())
+vp=[]
+kp=bin(2**num-1)[2::]
+lp=len(kp)
+for i in range(0,2**nump):
+	sp=bin(i)[2::]
+	if len(sp)<lp:
+		vp.append([sp.count("1"),(lp-len(sp))*"0"+sp])
+	else:
+		vp.append([sp.count("1"),sp])
+vp.sort()
+for i in range(0,len(vp)):
+        print(vp[i][1])
