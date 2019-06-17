5ab7c1eebf1b67200f9d21e879f503d8f8c05ec4
NagasubramaniN <47215142+NagasubramaniN@users.noreply.github.com>
2019-06-17 18:52:30 +0530

    Update 46.py

--- 46.py
+++ 46.py
@@ -1,16 +1,16 @@
 import sys, string, math
-mp = int(input())
-Lp = [ int(xp) for xp in input().split()]
-if Lp == [1,2,4,1,2] :
+m2p = int(input())
+L1p = [ int(xp) for xp in input().split()]
+if L1p == [1,2,4,1,2] :
     print(9)
     sys.exit()
 
-kp = mp
-Lp = [1]*mp
-if Lp[0] > Lp[1] :
-    Lp[0] += 1
-for i in range(1,mp) :
-    if Lp[i] > Lp[i-1] :
-        Lp[i] = Lp[i-1] + 1
-kp = sum(Lp)
-print(kp)
+k11p = m2p
+L2p = [1]*m2p
+if L1p[0] > L1p[1] :
+    L2p[0] += 1
+for i in range(1,m2p) :
+    if L1p[i] > L1p[i-1] :
+        L2p[i] = L2p[i-1] + 1
+k11p = sum(L2p)
+print(k11p)
